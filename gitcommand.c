d00414f230271ff9429291da911e8f6f70306184
toshi9685 <remighost@gmail.com>
2017-10-20 16:37:27 +0900

    ver1

---
+++ gitcommand.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void){
+  printf("git status : ワークツリーとインデックスの状態を確認する\n");
+  printf("git add <filename>   : インデックスに登録する\nスペース区切りで複数指定できる\n");
+  printf("git add . : 全てのファイルをインデックスに登録する\n");
+  return 0;
+}
